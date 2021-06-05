84f026f3fbd7527bc65c0851b81ac2377533cef0
mariusmihaicojocaru <marius_mihai_cojocaru@yahoo.ro>
2021-06-05 12:07:08 +0300

    final changes

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -1,4 +1,4 @@
 export class AppConfig{
-  public static API_PATH = 'http://localhost:8080';
+  public static API_PATH = 'https://morning-mesa-33291.herokuapp.com';
   public static AUTHORIZATION_HEADER = 'auth_header';
 }
